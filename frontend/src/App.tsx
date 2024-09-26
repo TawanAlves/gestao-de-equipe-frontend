e28d92f5de6e85c8c2d2ec643ea794548e9cb5e8
Tawan Alves <tas.tawan@gmail.com>
2024-09-26 11:41:21 -0300

    feat: start routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,20 @@ import Login from "./pages/Login/Login";
 import Membros from "./pages/Membros/Membros";
 import BaseScreen from "../src/components/Screen/BaseScreen"
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
 function App() {
   return (
     <div className="App">
-      <BaseScreen />
+      {/* <BaseScreen /> */}
+      <BrowserRouter>
+      <Routes>        
+          <Route path="/" element={<Login />} />
+          {/* <Route path="/base" element={<BaseScreen />} /> */}
+          <Route path="/membros" element={<Membros />} />      
+      </Routes>   
+    </BrowserRouter>
     </div>
   );
 }
