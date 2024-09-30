7a7bc69954923c9241b16fef80486a799b8e00f2
Tawan Alves <tas.tawan@gmail.com>
2024-09-30 11:19:43 -0300

    feat: using square box component at dashboard page

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
+//css
+import {ArrowRightIcon, HeaderTitle, DashboardContent } from '../Dashboard/DashboardCss'
 
 //components
 import RetangleBox  from '../../components/RetangleBox/RetangleBox';
 import BaseScreen from '../../components/Screen/BaseScreen'
+import SquareBox from '../../components/SquareBox/SquareBox';
 
-import {ArrowRightIcon, HeaderTitle } from '../Dashboard/DashboardCss'
 
 const Dashboard: React.FC = () => {
   return (
-   <BaseScreen header="Dashboard">   
-      <RetangleBox style={{borderRadius:"100px 10px 10px 100px", margin:"40px 0"}}>
-         <div></div>
-         <div><HeaderTitle> Visualização por Membros</HeaderTitle> </div>
-         <div><ArrowRightIcon /></div>
-         
-      </RetangleBox>   
+     <BaseScreen header="Dashboard">   
+      <div>
+         <RetangleBox style={{borderRadius:"100px 10px 10px 100px", margin:"40px 0"}}>
+            <div></div>
+            <div><HeaderTitle> Visualização por Membros</HeaderTitle> </div>
+            <div><ArrowRightIcon /></div>         
+         </RetangleBox>  
+         <DashboardContent>      
+            <SquareBox title='Total Mês Atual' aplicou={10} recebeu={10}/>
+            <SquareBox title='Total Mês Passado' aplicou={10} recebeu={10}/>
+         </DashboardContent>    
+         <DashboardContent>                 
+            <SquareBox title='Últimos 6 meses' aplicou={10} recebeu={10}/>
+            <SquareBox title='Geral' aplicou={10} recebeu={10}/>
+         </DashboardContent>     
+      </div>
    </BaseScreen>
   )
 }
--- frontend/src/pages/Dashboard/DashboardCss.tsx
+++ frontend/src/pages/Dashboard/DashboardCss.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { MdKeyboardArrowRight } from "react-icons/md";
 
 export const HeaderTitle = styled.p`
@@ -10,4 +10,10 @@ export const HeaderTitle = styled.p`
 export const ArrowRightIcon = styled(MdKeyboardArrowRight)`
    color: #000000 !important;
    font-size: 25px;
+`
+export const DashboardContent = styled.div`
+   display: flex;
+   justify-content: space-between;
+   flex-wrap: wrap;
+   padding-bottom: 22px;
 `
