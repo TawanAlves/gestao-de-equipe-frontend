2859063f7b6a52065c2a4afef990b99dc54324e5
Tawan Alves <tas.tawan@gmail.com>
2024-09-30 11:18:36 -0300

    feat: creat square box component

---
+++ frontend/src/components/SquareBox/SquareBoxCss.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const SquareBoxContainer = styled.div`
+   border-radius: 10px;
+   height: 90px;
+   width: 150px;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   box-shadow: 0 1px 4px rgba(0, 0, 0, 0.25);
+   padding: 11px;
+`
+
+export const SquareBoxContent = styled.div`
+   display: flex;
+   justify-content: space-between;
+`
+export const BoxTitle = styled.h1`
+   color: #005B64;
+   font-size: 14px; 
+   padding-bottom: 5px;
+`;
+
+export const LeftContent = styled.div`   
+  
+`;
+
+export const RightContent = styled.div`
+  
+`;
+
+export const ContentTitle = styled.p`
+   font-size: 13px;
+   color: #005B64;  
+   padding-bottom: 5px;
+`;
+
+export const Number = styled.p`
+   color: #005B64; 
+   font-size: 17px;
+   font-weight: 700;
+`;
